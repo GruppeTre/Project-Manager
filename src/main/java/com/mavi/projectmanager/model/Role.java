3f9d7efb1a1a716f6e6481dafd9143270d182bf4
MaVirgil <magnusvirgil@hotmail.com>
2025-11-21 12:49:25 +0100

    feat: add enum for account types

---
+++ src/main/java/com/mavi/projectmanager/model/Role.java
@@ -0,0 +1,15 @@
+package com.mavi.projectmanager.model;
+
+public enum Role {
+    ADMIN (1, "Admin"),
+    PROJECT_LEAD (2, "Project Lead"),
+    TEAM_MEMBER (3, "Team Member");
+
+    private int id;
+    private String value;
+
+    private Role (int id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+}
