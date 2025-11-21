21c8b87c79c879aa5fd26485bce993b07690b92d
MaVirgil <magnusvirgil@hotmail.com>
2025-11-21 12:49:53 +0100

    feat: add fields and methods for data models

--- src/main/java/com/mavi/projectmanager/model/Account.java
+++ src/main/java/com/mavi/projectmanager/model/Account.java
@@ -1,4 +1,49 @@
 package com.mavi.projectmanager.model;
 
 public class Account {
+
+    private int id;
+    private Role role;
+    private String password;
+    private Employee employee;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setEmployee(Employee emp) {
+        this.employee = emp;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return employee.getFirstName();
+    }
+
+    public String getLastName() {
+        return employee.getLastName();
+    }
+
+    public String getMail() {
+        return employee.getMail();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/mavi/projectmanager/model/Employee.java
+++ src/main/java/com/mavi/projectmanager/model/Employee.java
@@ -1,6 +1,49 @@
 package com.mavi.projectmanager.model;
 
 public class Employee {
-    int id;
-    String name;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String mail;
+    private String position;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
