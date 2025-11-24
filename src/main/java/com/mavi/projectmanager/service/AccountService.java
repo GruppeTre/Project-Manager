13be9351da29247b8afccf92ee0610161d9b3270
WiiTee <somewhereinnowhere@live.dk>
2025-11-24 11:15:15 +0100

    feat: Added getAccountByID method to AccountService

--- src/main/java/com/mavi/projectmanager/service/AccountService.java
+++ src/main/java/com/mavi/projectmanager/service/AccountService.java
@@ -1,4 +1,14 @@
 package com.mavi.projectmanager.service;
 
+import com.mavi.projectmanager.model.Account;
+import com.mavi.projectmanager.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+    private AccountRepository accountRepository;
+
+    public Account getAccountByID(int id){
+        return accountRepository.getAccountByID(id);
+    }
 }
