352179523a8ba6163508226e65ec68c674b3cb5f
WiiTee <somewhereinnowhere@live.dk>
2025-11-24 12:01:41 +0100

    feat: Added getEmployeeByID method to AccountRepository

--- src/main/java/com/mavi/projectmanager/repository/AccountRepository.java
+++ src/main/java/com/mavi/projectmanager/repository/AccountRepository.java
@@ -12,6 +12,26 @@ import org.springframework.stereotype.Repository;
 public class AccountRepository {
     private JdbcTemplate jdbcTemplate;
 
+    public final RowMapper<Account> accountRowMapper = ((rs, rowNum) -> {
+        Account account = new Account();
+        account.setId(rs.getInt("id"));
+        account.setRole(Role.values()[rs.getInt("role")]);
+        account.setPassword(rs.getString("password"));
+        account.setEmployee(getEmployeeByID());
+
+        return account;
+    });
+
+
+    public Employee getEmployeeByID(int id){
+        String query = "SELECT * FROM Employee WHERE id = ?";
+
+        try{
+            return jdbcTemplate.queryForObject(query, employeeRowMapper, id);
+        } catch (EmptyResultDataAccessException e){
+            return null;
+        }
+    }
     public Account getAccountByID(int id){
         String query= "SELECT * FROM Account WHERE id = ?";
 
