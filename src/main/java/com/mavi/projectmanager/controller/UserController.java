b56a37f8f73e3ac2619d6a8e94369d65823c4784
WiiTee <somewhereinnowhere@live.dk>
2025-11-24 11:12:04 +0100

    feat: Added controller method getEditUser for editUserPage

--- src/main/java/com/mavi/projectmanager/controller/UserController.java
+++ src/main/java/com/mavi/projectmanager/controller/UserController.java
@@ -1,10 +1,28 @@
 package com.mavi.projectmanager.controller;
 
-import com.mavi.projectmanager.exception.Field;
+import com.mavi.projectmanager.model.Account;
+import com.mavi.projectmanager.model.Role;
+import com.mavi.projectmanager.service.AccountService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    private AccountService accountService;
+
+    @GetMapping("edit/{id}")
+    public String getEditUser(@PathVariable int id, Model model, HttpSession httpSession){
+        Account account = accountService.getAccountByID();
+
+        model.addAttribute("account", account);
+        model.addAttribute("roles", Role.values());
+
+        return "editUserPage";
+    }
 }
