9ac3fd5e75883eb29e3acc53165ca23e0b35c871
WiiTee <somewhereinnowhere@live.dk>
2025-11-24 11:56:33 +0100

    feat: Added getAccountByID method AccountRepository

--- src/main/java/com/mavi/projectmanager/repository/AccountRepository.java
+++ src/main/java/com/mavi/projectmanager/repository/AccountRepository.java
@@ -1,4 +1,24 @@
 package com.mavi.projectmanager.repository;
 
+import com.mavi.projectmanager.model.Account;
+import com.mavi.projectmanager.model.Employee;
+import com.mavi.projectmanager.model.Role;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AccountRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public Account getAccountByID(int id){
+        String query= "SELECT * FROM Account WHERE id = ?";
+
+        try{
+            return jdbcTemplate.queryForObject(query, accountRowMapper, id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
 }
