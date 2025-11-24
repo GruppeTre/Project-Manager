9e96b1a68ebf2db36ea8efb9684433724d8e367c
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2025-11-24 12:27:38 +0100

    refactor: fixes controller method for creating user

--- src/main/java/com/mavi/projectmanager/controller/UserController.java
+++ src/main/java/com/mavi/projectmanager/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mavi.projectmanager.controller;
 
 import com.mavi.projectmanager.exception.Field;
+import com.mavi.projectmanager.exception.InvalidFieldException;
 import com.mavi.projectmanager.model.Account;
 import com.mavi.projectmanager.service.AccountService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.mavi.projectmanager.controller.utils.SessionUtils;
 
 
 @Controller
@@ -22,29 +24,36 @@ public class UserController {
         this.service = service;
     }
 
-
     //Shows the createUSerPage
     @GetMapping("/create")
-    public String getCreateUserPage(Model model) {
+    public String getCreateUserPage(HttpSession session, Model model) {
+
+        if (!SessionUtils.isLoggedIn(session)) {
+            return "redirect:/";
+        }
+
         Account accountToAdd = new Account();
         model.addAttribute("accountToAdd", accountToAdd);
 
         return "createUserPage";
     }
 
-
     //Creates a new account
     @PostMapping("/create")
     public String createNewUser(HttpSession session, Model model, @ModelAttribute Account newAccount, HttpServletResponse response) {
 
+        if (!SessionUtils.isLoggedIn(session)) {
+            return "redirect:/";
+        }
+
         //Check to see if all fields are filled correctly
         try{
-            newAccount = service.createAccount(newAccount);
+            newAccount = service.createUser(newAccount);
             session.setAttribute("account", newAccount);
-        } catch (InvalidFieldsException e) {
+        } catch (InvalidFieldException e) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             model.addAttribute("error", true);
-            model.addAttribute("invalidField", e.getIncorrectField());
+            model.addAttribute("InvalidField", e.getField());
             model.addAttribute("newUser", newAccount);
             return "createUserPage";
         }
