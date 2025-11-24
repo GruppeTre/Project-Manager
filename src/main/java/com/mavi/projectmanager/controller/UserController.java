8e19c00d405530abc6a58d9d4e2e1968fc7b3cbc
404BrainNotFound1000 <emilgurres@gmail.com>
2025-11-24 12:54:33 +0100

    feat: GetMethod for retrieving view page

--- src/main/java/com/mavi/projectmanager/controller/UserController.java
+++ src/main/java/com/mavi/projectmanager/controller/UserController.java
@@ -1,6 +1,42 @@
 package com.mavi.projectmanager.controller;
 
 import com.mavi.projectmanager.exception.Field;
+import com.mavi.projectmanager.model.Account;
+import com.mavi.projectmanager.service.AccountService;
+import com.mavi.projectmanager.service.EmployeeService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Controller
+@RequestMapping("user")
 public class UserController {
+    private final AccountService;
+    private final EmployeeService;
+
+    @Autowired
+    public UserController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    private boolean isLoggedIn = false;
+
+    @GetMapping
+    public showIndex(HttpSession session, Model model) {
+
+        if (!sessionUtils.isloggedIn(session)) {
+            List<Account> accounts = accountService.METHODTORETRIEVEACCOUNTS;
+            model.addAttribute(accounts);
+            return "index";
+        }
+        else {
+            return "index";
+        }
+    }
+
 }
