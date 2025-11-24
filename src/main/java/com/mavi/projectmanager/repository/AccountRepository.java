298843576a422665d9a9a3de6b140cb1ca4a8b76
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2025-11-24 11:00:03 +0100

    feat: add repository method for inserting account

--- src/main/java/com/mavi/projectmanager/repository/AccountRepository.java
+++ src/main/java/com/mavi/projectmanager/repository/AccountRepository.java
@@ -1,4 +1,53 @@
 package com.mavi.projectmanager.repository;
 
+import com.mavi.projectmanager.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Repository
 public class AccountRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public AccountRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    //Inserts an account in the database
+    public Account createAccount(Account account) {
+
+        String query = "INSERT IGNORE INTO account (id, role, password, employee) VALUES (?,?,?,?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        try {
+            jdbcTemplate.update(connection -> {
+                PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, account.getId());
+                ps.setObject(1, account.getRole());
+                ps.setString(2, account.getPassword());
+                ps.setString(3, account.getFirstName());
+                ps.setString(4, account.getLastName());
+                ps.setString(4, account.getMail());
+
+                return ps;
+            }, keyHolder);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        //Returns the keyholder for check
+        if (keyHolder.getKey() == null) {
+            throw new RuntimeException("Failed to obtain generated key for new account");
+        }
+
+        account.setId(keyHolder.getKey().intValue());
+
+        return account;
+    }
 }
