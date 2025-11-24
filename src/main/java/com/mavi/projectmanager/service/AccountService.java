0c3c4d0b815bb1b8cbbb7e566b731ed8c2135217
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2025-11-24 11:31:07 +0100

    refactor: missing bean for service

--- src/main/java/com/mavi/projectmanager/service/AccountService.java
+++ src/main/java/com/mavi/projectmanager/service/AccountService.java
@@ -2,7 +2,9 @@ package com.mavi.projectmanager.service;
 
 import com.mavi.projectmanager.model.Account;
 import com.mavi.projectmanager.repository.AccountRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AccountService {
 
     private AccountRepository accountRepository;
