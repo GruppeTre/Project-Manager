190c54658d2aea775effbe74f67c4fe61e140666
WiiTee <somewhereinnowhere@live.dk>
2025-11-24 12:28:06 +0100

    refactor: Refactored editUserPage to use localization file

--- src/main/java/com/mavi/projectmanager/controller/UserController.java
+++ src/main/java/com/mavi/projectmanager/controller/UserController.java
@@ -4,6 +4,7 @@ import com.mavi.projectmanager.model.Account;
 import com.mavi.projectmanager.model.Role;
 import com.mavi.projectmanager.service.AccountService;
 import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private AccountService accountService;
+
+    public UserController (AccountService accountService){
+        this.accountService = accountService;
+    }
 
     @GetMapping("edit/{id}")
     public String getEditUser(@PathVariable int id, Model model, HttpSession httpSession){
