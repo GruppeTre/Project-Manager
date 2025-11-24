d31a383ffa0f27585d8324e64d2f279698fb921b
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2025-11-24 10:46:44 +0100

    feat: add service method for creating account

--- src/main/java/com/mavi/projectmanager/service/AccountService.java
+++ src/main/java/com/mavi/projectmanager/service/AccountService.java
@@ -1,4 +1,16 @@
 package com.mavi.projectmanager.service;
 
+import com.mavi.projectmanager.model.Account;
+import com.mavi.projectmanager.repository.AccountRepository;
+
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    //Registers a user
+    public Account createAccount(Account account) {
+
+        return accountRepository.createAccount(account);
+    }
+
 }
