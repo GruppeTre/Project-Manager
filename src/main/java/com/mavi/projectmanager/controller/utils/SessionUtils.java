d0b612a3fdcbe235e2f26f506b2b607e9af449ac
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2025-11-24 12:28:18 +0100

    feat: add method for checking if logged in

---
+++ src/main/java/com/mavi/projectmanager/controller/utils/SessionUtils.java
@@ -0,0 +1,11 @@
+package com.mavi.projectmanager.controller.utils;
+
+import jakarta.servlet.http.HttpSession;
+
+public class SessionUtils {
+
+    //Method for login that can be used across all Controllers
+    public static boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("user") != null;
+    }
+}
