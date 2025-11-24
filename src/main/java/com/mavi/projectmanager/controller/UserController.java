58f585b5ae9c696c36f7f0334190b39057dfe487
WiiTee <somewhereinnowhere@live.dk>
2025-11-24 10:53:59 +0100

    feat: Added @Controller and @RequestMapping(user) to UserController

--- src/main/java/com/mavi/projectmanager/controller/UserController.java
+++ src/main/java/com/mavi/projectmanager/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.mavi.projectmanager.controller;
 
 import com.mavi.projectmanager.exception.Field;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("user")
 public class UserController {
 }
