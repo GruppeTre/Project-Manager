f54fda9e1fe6b3fed985d85fc510608888289666
404BrainNotFound1000 <emilgurres@gmail.com>
2025-11-25 11:35:24 +0100

    feat: Several features added

--- src/main/java/com/mavi/projectmanager/controller/UserController.java
+++ src/main/java/com/mavi/projectmanager/controller/UserController.java
@@ -1,42 +1,36 @@
 package com.mavi.projectmanager.controller;
 
-import com.mavi.projectmanager.exception.Field;
-import com.mavi.projectmanager.model.Account;
 import com.mavi.projectmanager.service.AccountService;
-import com.mavi.projectmanager.service.EmployeeService;
+import com.mavi.projectmanager.service.ProjectService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
-@RequestMapping("user")
+@RequestMapping("/")
 public class UserController {
-    private final AccountService;
-    private final EmployeeService;
 
-    @Autowired
+    private AccountService accountService;
+
+    private ProjectService projectService;
+
+    //Note: there can only be one constructor, as Spring only wants a single autowiring per class.
     public UserController(AccountService accountService) {
         this.accountService = accountService;
+        this.projectService = projectService;
     }
 
-    private boolean isLoggedIn = false;
+    @GetMapping("/overview")
+    public String getOverviewPage(HttpSession session, Model model) {
 
-    @GetMapping
-    public showIndex(HttpSession session, Model model) {
+        /*if (!sessionUtils.isloggedIn(session)) {
+            return "redirect:/login";
+        }*/
+        model.addAttribute("users", accountService.getAllAccounts());
 
-        if (!sessionUtils.isloggedIn(session)) {
-            List<Account> accounts = accountService.METHODTORETRIEVEACCOUNTS;
-            model.addAttribute(accounts);
-            return "index";
-        }
-        else {
-            return "index";
-        }
+        return "overviewPage";
     }
-
 }
--- src/main/java/com/mavi/projectmanager/repository/AccountRepository.java
+++ src/main/java/com/mavi/projectmanager/repository/AccountRepository.java
@@ -1,4 +1,16 @@
 package com.mavi.projectmanager.repository;
 
+
+import com.mavi.projectmanager.model.Account;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class AccountRepository {
+
+    public List<Account> getAllAccounts() {
+        return null;
+    }
+
 }
