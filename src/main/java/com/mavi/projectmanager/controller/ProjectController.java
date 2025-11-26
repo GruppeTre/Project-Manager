5d67b01ef174f97af973a00159606479f81c890a
WiiTee <somewhereinnowhere@live.dk>
2025-11-26 21:31:33 +0100

    feat: Added getProjectOverviewPage to ProjectController

--- src/main/java/com/mavi/projectmanager/controller/ProjectController.java
+++ src/main/java/com/mavi/projectmanager/controller/ProjectController.java
@@ -1,4 +1,34 @@
 package com.mavi.projectmanager.controller;
 
+import com.mavi.projectmanager.controller.utils.SessionUtils;
+import com.mavi.projectmanager.service.ProjectService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/overview")
 public class ProjectController {
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService){
+        this.projectService = projectService;
+    }
+
+    public String getProjectOverviewPage(@RequestParam("viewMode") String viewMode, @RequestParam("perm") int perm, Model model, HttpSession session){
+        if(!SessionUtils.isLoggedIn(session)){
+            return "redirect:/";
+        }
+
+        if(viewMode.equals("projects") && perm == 1){
+            model.addAttribute("projects", projectService.getProjects());
+
+            return "overviewPage";
+        } else {
+            return "overviewPage";
+        }
+
+    }
 }
