edcd5e7d7597efda50bf0eb9666c4385e2f852ba
Magnus Sørensen <magnusvirgil@hotmail.com>
2025-12-02 13:20:13 +0100

    Revert "98-som-systemadministrator-vil-jeg-kunne-slette-et-projekt"

--- src/main/java/com/mavi/projectmanager/service/ProjectService.java
+++ src/main/java/com/mavi/projectmanager/service/ProjectService.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ProjectService {
@@ -69,32 +68,9 @@ public class ProjectService {
         return true;
     }
 
-    public List<Project> getProjects() {
+    public List<Project> getProjects(){
         return projectRepository.getProjects();
     }
 
-    public int deleteProjectByProject(Project toDelete) {
-        //Check if Object id has the value 0 or below 0.
-        if (toDelete == null || toDelete.getId() <= 0) {
-            throw new IllegalArgumentException("Invalid project provided for deletion.");
-        }
-
-        // Fetch the latest project from the repository to validate it exists
-        Project existingProject = projectRepository.getProjectById(toDelete.getId());
-        if (existingProject == null) {
-            // Project not found
-            return 0;
-        }
-        //Validate that the project to be deleted matches the project designated by id in toDelete - perform deletion.
-        if (Objects.equals(toDelete.getName(), existingProject.getName())
-                && Objects.equals(toDelete.getStart_date(), existingProject.getStart_date())
-                && Objects.equals(toDelete.getEnd_date(), existingProject.getEnd_date())) {
-            return projectRepository.deleteProjectByProject(existingProject);
-        }
-        else {
-            throw new IllegalStateException("Project details on this project does not match a project in the database");
-
-        }
-    }
 
 }
